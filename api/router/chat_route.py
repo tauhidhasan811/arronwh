93d00b3fe215c1f7e024de63c24d26e83d755f18
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-04-17 09:27:14 +0600

    reverse previous chat

--- api/router/chat_route.py
+++ api/router/chat_route.py
@@ -1,25 +1,26 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from api.schemas.chat_body import Chatbody
+from src.services.data_processor import DataProcessor
 
+data_processor = DataProcessor()
 
 router = APIRouter(prefix='/api/ai', tags=['Chat With AI'])
 
 @router.post('/chatbot')
 async def chat_with_ai(body: Chatbody):
     previous_chat = body.previous_chat
+    selected_chat = data_processor.process_previous_history(previous_data=previous_chat)
     user_query = body.user_query
     print(type(previous_chat))
-    # response = JSONResponse(
-    #     status_code=200,
-    #     content={
-    #         'status': True,
-    #         'status_code': 200,
-    #         'response': {
-    #             "data": body,
-    #                      'user_query': user_query}
-    #     }
-    # )
-    # return response
+    response = JSONResponse(
+        status_code=200,
+        content={
+            'status': True,
+            'status_code': 200,
+            'response': selected_chat
+        }
+    )
+    return response
 
-    return body
+    # return body
--- src/services/data_processor.py
+++ src/services/data_processor.py
@@ -10,26 +10,28 @@ class DataProcessor:
     
     @staticmethod
     def process_previous_history(previous_data:List[Dict]):
-        min_chat = 5
-        max_token = 1000
+        min_chat = 2
+        max_token = 100
         token_count = 0
         selected_chat = []
-        idx = 0
+        idx = 1
         for data in reversed(previous_data):
-            idx+=1
             prev = str(data)
             current_token = len(prev)
             if token_count <= max_token or idx <= min_chat:
+                print(f"Current chat count : {idx} && token count: {token_count}")
                 token_count += current_token
-                selected_chat.append(data)
+                selected_chat.append(data.model_dump())
+                idx+=1
             
             else:
                 break
         
         print('-' * 60)
-        print(' ' * 20, 'Chat History process successfully.')
+        print(' ' * 10, 'Chat History process successfully.')
         print('-' * 60)
         print(f"Total Number of token : {token_count}")
         print(f"Total Chat taken      : {idx}")
-        return selected_chat
+
+        return list(reversed(selected_chat))
     
