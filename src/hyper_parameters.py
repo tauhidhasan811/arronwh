4e9f13d93cc03ecd57ed89611bbee4f6d51abc8e
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-05-06 09:32:27 +0600

    store chunk and embedding on database

--- src/hyper_parameters.py
+++ src/hyper_parameters.py
@@ -3,6 +3,7 @@ params = {
     'model_name': 'gpt-4.1-2025-04-14',
     'knowledge_path': r'data\files\YoloHeat Company Guide.docx',
     'chromadb_path': r'data\chroma_db',
+    'client_name': 'all_data', 
     'collections': {
         'products': {
             'exclude_field' : ['images', 'includedImages', 'featureInformation.featureLogo', 'user', 'createdAt', 'updatedAt', '__v']
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from src.services.data_processor import DataProcessor
+from src.config.config_chromadb import ChromaDB
 
 path = r'data\files\YoloHeat Company Guide.docx'
 
 chunk = DataProcessor.create_chunk(path)
 
-data = "hi !! how are you??"
+data = ["hi !! how are you??"]
 
 ebd = DataProcessor.embedde_sentence(data)
 print(ebd.shape)
 # print(chunk)
+
+chroma_db = ChromaDB()
+
+chroma_db.store_knowledge(chunk=data, embedding=ebd)
